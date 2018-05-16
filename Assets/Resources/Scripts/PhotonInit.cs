f4aaac8bd33b1c0a1421ec98a0e590c1e83f6f7d
VorbyT <why1noob@gmail.com>
2018-05-16 23:17:22 +0300

    init

---
+++ Assets/Resources/Scripts/PhotonInit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonInit : Photon.MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("1");
+        PhotonNetwork.autoJoinLobby = true;
+        Debug.Log(1);
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log(1.5);
+        PhotonNetwork.JoinOrCreateRoom("IgorianovaZhopa", new RoomOptions(), TypedLobby.Default);
+        Debug.Log(2);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("SoDPlayer", new Vector3(0.22f, 0.39f, 2.94f), Quaternion.identity, 0);
+        Debug.Log(3);
+    }
+}
---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : Photon.MonoBehaviour {
+
+    public LayerMask Clickable;
+
+    private NavMeshAgent Agent;
+
+    private void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (!photonView.isMine) return;
+        if (Input.GetMouseButton(0))
+        {
+            var camToCursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(camToCursorRay,out hitInfo, 100f, Clickable))
+            {
+                Agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        Debug.Log(Agent.transform.position);
+        var currentHeroPosition = Agent.transform.position;
+        var currentHeroRotation = Agent.transform.rotation;
+        stream.Serialize(ref currentHeroPosition);
+        stream.Serialize(ref currentHeroRotation);
+        if (stream.isReading)
+        {
+            Agent.transform.position = currentHeroPosition;
+            Agent.transform.rotation = currentHeroRotation;
+        }
+        Debug.Log(currentHeroPosition);
+    }
+}
